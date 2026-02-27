35464b0ff696bc1ef94a3ece39128eceeae39666
Zfike <97480418+Zfike@users.noreply.github.com>
2026-02-27 06:58:23 -0600

    Added base files from course

---
+++ ui.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
+)
+
+func (app *Config) makeUI() {
+	// get the current price of gold
+	openPrice, currentPrice, priceChange := app.getPriceText()
+
+	// put price information into a container
+	priceContent := container.NewGridWithColumns(3,
+		openPrice,
+		currentPrice,
+		priceChange,
+	)
+
+	app.PriceContainer = priceContent
+
+	// get toolbar
+	toolBar := app.getToolBar()
+	app.ToolBar = toolBar
+
+	// get app tabs
+	tabs := container.NewAppTabs(
+		container.NewTabItemWithIcon("Prices", theme.HomeIcon(), canvas.NewText("Price content goes here", nil)),
+		container.NewTabItemWithIcon("Holdings", theme.InfoIcon(), canvas.NewText("Holdings content goes here", nil)),
+	)
+	tabs.SetTabLocation(container.TabLocationTop)
+
+	// add container to window
+	finalContent := container.NewVBox(priceContent, toolBar, tabs)
+
+	app.MainWindow.SetContent(finalContent)
+}
